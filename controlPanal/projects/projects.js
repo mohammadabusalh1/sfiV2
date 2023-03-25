83ecdf08a008f4ad9f3ecede8d40be2bce18d067
tis <abusalhm106@gmail.com>
2023-03-25 13:45:50 +0300

    v2.1.2

--- controlPanal/projects/projects.js
+++ controlPanal/projects/projects.js
@@ -159,6 +159,570 @@ $(document).ready(function () {
   });
   //=============================================
 
+  // get all areas name to select when crate projects
+  function areaReload(sql) {
+    $.ajax({
+      url: "../phpFile/show.php",
+      data: { sql: sql },
+      dataType: "json",
+      type: "post",
+      success: function (data) {
+        ht = "";
+        for (i = 0; i < data.length; i++) {
+          ht +=
+            '<option value="' +
+            data[i].area_name +
+            '">' +
+            data[i].area_name +
+            "</option>";
+        }
+        $("#area").html(ht);
+      },
+    });
+  }
 
-  
+  areaReload("SELECT * FROM `area`");
+  //=============================================
+
+  // add all areas that inserted to array
+  let areasArray = [];
+
+  const areasInsertedDiv = $("#areaInserted");
+
+  $("#addArea").click(function () {
+    const area = $("#area").val();
+
+    // Check if area is already in areaArray
+    if (!areasArray.includes(area)) {
+      areasArray.push(area);
+
+      // Map AreaArray to an array of HTML elements
+      const newAreaArray = areasArray.map((e) => {
+        const div = $("<div>");
+        const h6 = $("<h6>").text(e);
+        const button = $("<button>")
+          .text("حذف")
+          .addClass("removeArea")
+          .data("id", e);
+        div.append(h6, button);
+        return div;
+      });
+
+      // Add the new HTML elements to areasInsertedDiv
+      areasInsertedDiv.html(newAreaArray);
+      areasInsertedDiv.show();
+    }
+  });
+
+  // Add a click event listener to the document, and check if the clicked element is a removeGoal button
+  $(document).on("click", ".removeArea", function () {
+    const id = $(this).data("id");
+
+    // Remove the corresponding goal from goalsArray
+    areasArray = areasArray.filter((e) => e !== id);
+
+    // Remove the corresponding HTML element from goalsInsertedDiv
+    $(this).parent().remove();
+
+    // Hide goalsInsertedDiv if there are no goal left
+    if (areasArray.length === 0) {
+      areasInsertedDiv.hide();
+    }
+  });
+  //=============================================
+
+  // get all areas name to select when crate projects
+  function targetReload(sql) {
+    $.ajax({
+      url: "../phpFile/show.php",
+      data: { sql: sql },
+      dataType: "json",
+      type: "post",
+      success: function (data) {
+        ht = "";
+        for (i = 0; i < data.length; i++) {
+          ht +=
+            '<option value="' +
+            data[i].target_group +
+            '">' +
+            data[i].target_group +
+            "</option>";
+        }
+        $("#target").html(ht);
+      },
+    });
+  }
+
+  targetReload("SELECT * FROM `target_groups`");
+  //=============================================
+
+  // add all target that inserted to array
+  let targetsArray = [];
+
+  const targetsInsertedDiv = $("#targetInserted");
+
+  $("#addTarget").click(function () {
+    const target = $("#target").val();
+
+    // Check if area is already in targetsArray
+    if (!targetsArray.includes(target)) {
+      targetsArray.push(target);
+
+      // Map targetsArray to an array of HTML elements
+      const newTargetArray = targetsArray.map((e) => {
+        const div = $("<div>");
+        const h6 = $("<h6>").text(e);
+        const button = $("<button>")
+          .text("حذف")
+          .addClass("removeTarget")
+          .data("id", e);
+        div.append(h6, button);
+        return div;
+      });
+
+      // Add the new HTML elements to targetsInsertedDiv
+      targetsInsertedDiv.html(newTargetArray);
+      targetsInsertedDiv.show();
+    }
+  });
+
+  // Add a click event listener to the document, and check if the clicked element is a removeGoal button
+  $(document).on("click", ".removeTarget", function () {
+    const id = $(this).data("id");
+
+    // Remove the corresponding goal from goalsArray
+    targetsArray = targetsArray.filter((e) => e !== id);
+
+    // Remove the corresponding HTML element from goalsInsertedDiv
+    $(this).parent().remove();
+
+    // Hide goalsInsertedDiv if there are no goal left
+    if (targetsArray.length === 0) {
+      targetsInsertedDiv.hide();
+    }
+  });
+  //=============================================
+
+  function add(sql) {
+    $.ajax({
+      url: "../phpFile/add.php",
+      data: { sqlAdd: sql },
+      type: "post",
+      success: function (out) {},
+    });
+  }
+
+  function reload(sql) {
+    $.ajax({
+      url: "../phpFile/show.php",
+      data: { sql: sql },
+      dataType: "json",
+      type: "post",
+      success: function (data) {
+        ht =
+          "<tr> <th>إسم المشروع</th> <th>قيمة المشروع</th> <th>فكرة المشروع</th> <th>تاريخ البدء</th> <th>تاريخ الانتهاء</th> <th> الحذف</th> <th> تعديل</th></tr>";
+        for (i = 0; i < data.length; i++) {
+          ht +=
+            "<tr><td>" +
+            data[i].project_name +
+            "</td><td>" +
+            data[i].project_value +
+            " " +
+            data[i].value_type +
+            "</td><td>" +
+            data[i].project_idea +
+            "</td><td>" +
+            data[i].start_date +
+            "</td><td>" +
+            data[i].end_date +
+            "</td> <td><button data-id= '" +
+            data[i].project_name +
+            '\' class="remove-btn">حذف <i class="fa fa-remove" aria-hidden="true"></i></button></td> <td><button data-id= \'' +
+            data[i].project_name +
+            '\' class="edit-btn">تعديل <i class="fas fa-edit"></i></button></td></tr>';
+        }
+        $(".table").html(ht);
+      },
+    });
+  }
+  reload("SELECT * FROM `project`");
+
+  $("#add").on("click", function () {
+    // Check if any of the input values are empty
+    if (
+      $("#name").val() === "" ||
+      $("#value").val() === "" ||
+      $("#idea").val() === "" ||
+      $("#startDate").val() === "" ||
+      $("#endDate").val() === ""
+    ) {
+      $("#alert").text("يرجى ملء جميع الحقول !");
+
+      // Check if any of the input values are empty
+      if ($("#name").val() === "") {
+        $("#name").css("border-color", "red");
+      } else {
+        $("#name").css("border-color", "");
+      }
+
+      if ($("#value").val() === "") {
+        $("#value").css("border-color", "red");
+      } else {
+        $("#value").css("border-color", "");
+      }
+
+      if ($("#idea").val() === "") {
+        $("#idea").css("border-color", "red");
+      } else {
+        $("#idea").css("border-color", "");
+      }
+
+      if ($("#startDate").val() === "") {
+        $("#startDate").css("border-color", "red");
+      } else {
+        $("#startDate").css("border-color", "");
+      }
+
+      if ($("#endDate").val() === "") {
+        $("#endDate").css("border-color", "red");
+      } else {
+        $("#endDate").css("border-color", "");
+      }
+
+      return false;
+    } else {
+      var name = $("#name").val();
+      var value = $("#value").val();
+      var valueType = $("#valueType").val();
+      var idea = $("#idea").val();
+      var startDate = $("#startDate").val();
+      var endDate = $("#endDate").val();
+
+      sqlAdd =
+        "INSERT INTO `project` (`project_name`, `project_value`, `project_idea`, `value_type`, `start_date`, `end_date`) VALUES ('" +
+        name +
+        "','" +
+        value +
+        "','" +
+        idea +
+        "','" +
+        valueType +
+        "','" +
+        startDate +
+        "','" +
+        endDate +
+        "')";
+
+      $.ajax({
+        url: "../phpFile/add.php",
+        data: { sqlAdd: sqlAdd },
+        type: "post",
+        success: function (out) {
+          if (out == "successfully") {
+            if (finsArray.length > 0) {
+              for (i = 0; i < finsArray.length; i++) {
+                let sql =
+                  "INSERT INTO `fin_pro`(`project_financier_name`, `project_name`) VALUES ('" +
+                  finsArray[i] +
+                  "','" +
+                  name +
+                  "')";
+                add(sql);
+              }
+            }
+
+            if (goalsArray.length > 0) {
+              for (i = 0; i < goalsArray.length; i++) {
+                let sql =
+                  "INSERT INTO `goal_pro`(`goal_name`, `project_name`) VALUES ('" +
+                  goalsArray[i] +
+                  "','" +
+                  name +
+                  "')";
+                add(sql);
+              }
+            }
+
+            if (areasArray.length > 0) {
+              for (i = 0; i < areasArray.length; i++) {
+                let sql =
+                  "INSERT INTO `pro_area`(`area_name`, `project_name`) VALUES ('" +
+                  areasArray[i] +
+                  "','" +
+                  name +
+                  "')";
+                add(sql);
+              }
+            }
+
+            if (targetsArray.length > 0) {
+              for (i = 0; i < targetsArray.length; i++) {
+                let sql =
+                  "INSERT INTO `targ_pro`(`target_group`, `project_name`) VALUES ('" +
+                  targetsArray[i] +
+                  "','" +
+                  name +
+                  "')";
+                add(sql);
+              }
+            }
+
+            $("#alert").text("تمت الأضافة");
+            $("input").val("");
+            finsArray = [];
+            goalsArray = [];
+            areasArray = [];
+            targetsArray = [];
+            $(".inserted").hide();
+          } else {
+            alert(out);
+          }
+        },
+      });
+    }
+  });
+
+  $("#tableDis input").keyup(function () {
+    val = $(this).val();
+    if (val == "") {
+      sql = "SELECT * FROM `project`";
+    } else {
+      sql =
+        "SELECT * FROM `project` WHERE `project_name` like '%" +
+        val +
+        "%' || `project_value` like '%" +
+        val +
+        "%' || `project_idea` like '%" +
+        val +
+        "%' || `value_type` like '%" +
+        val +
+        "%' || `start_date` like '%" +
+        val +
+        "%' || `end_date` like '%" +
+        val +
+        "%'";
+    }
+    reload(sql);
+  });
+  //=============================================
+
+  function deleteRow(sql) {
+    $.ajax({
+      url: "../phpFile/remove.php",
+      data: { sql: sql },
+      type: "post",
+      success: function (out) {},
+    });
+  }
+
+  $("#table").on("click", ".remove-btn", function () {
+    let n = confirm("تأكيد الحذف");
+
+    if (n) {
+      id = $(this).data("id");
+      table = "project";
+      feild = "project_name";
+      let sql =
+        "DELETE FROM `" + table + "` WHERE `" + feild + "` = '" + id + "'";
+      let sql1 = "DELETE FROM `fin_pro` WHERE `" + feild + "` = '" + id + "'";
+      let sql2 = "DELETE FROM `goal_pro` WHERE `" + feild + "` = '" + id + "'";
+      let sql3 = "DELETE FROM `pro_area` WHERE `" + feild + "` = '" + id + "'";
+      let sql4 = "DELETE FROM `targ_pro` WHERE `" + feild + "` = '" + id + "'";
+      deleteRow(sql);
+      deleteRow(sql1);
+      deleteRow(sql2);
+      deleteRow(sql3);
+      deleteRow(sql4);
+
+      let sql12 =
+        "UPDATE `activities` SET `project_name`='' WHERE `project_name`='" +
+        id +
+        "';";
+      $.ajax({
+        url: "../phpFile/update.php",
+        data: { sql: sql12 },
+        dataType: "json",
+        type: "post",
+        success: function (data) {},
+      });
+
+      sql = "SELECT * FROM `project`";
+      reload(sql);
+    }
+  });
+
+  //=============================================
+
+  // add event listener to edit button
+  $(document).on("click", ".edit-btn", function () {
+    // get project name from button data-id attribute
+    var projectName = $(this).data("id");
+
+    $("#edit").toggle();
+    $("#add").toggle();
+    // make AJAX call to fetch project information
+    $.ajax({
+      url: "../phpFile/show.php",
+      data: {
+        sql:
+          "SELECT * FROM `project` WHERE `project_name` = '" +
+          projectName +
+          "'",
+      },
+      dataType: "json",
+      type: "post",
+      success: function (data) {
+        // set input field values
+        $("#name").val(data[0].project_name);
+        $("#value").val(data[0].project_value);
+        $("#valueType").val(data[0].value_type);
+        $("#idea").val(data[0].project_idea);
+        $("#startDate").val(data[0].start_date);
+        $("#endDate").val(data[0].end_date);
+
+        var sqlFinanciers =
+          "SELECT `project_financier_name` FROM `fin_pro` WHERE `project_name` = '" +
+          projectName +
+          "'";
+        $.ajax({
+          url: "../phpFile/show.php",
+          data: { sql: sqlFinanciers },
+          dataType: "json",
+          type: "post",
+          success: function (out) {
+            for (let i = 0; i < out.length; i++) {
+              finsArray.push(out[i].project_financier_name);
+            }
+            // Map finsArray to an array of HTML elements
+            const newFinsArray = finsArray.map((e) => {
+              const div = $("<div>");
+              const h6 = $("<h6>").text(e);
+              const button = $("<button>")
+                .text("حذف")
+                .addClass("removeFin")
+                .data("id", e);
+              div.append(h6, button);
+              return div;
+            });
+
+            // Add the new HTML elements to finsInsertedDiv
+            finsInsertedDiv.html(newFinsArray);
+            finsInsertedDiv.show();
+          },
+        });
+
+        var sqlGoals =
+          "SELECT `goal_name` FROM `goal_pro` WHERE `project_name` = '" +
+          projectName +
+          "'";
+        $.ajax({
+          url: "../phpFile/show.php",
+          data: { sql: sqlGoals },
+          dataType: "json",
+          type: "post",
+          success: function (out) {
+            for (let i = 0; i < out.length; i++) {
+              goalsArray.push(out[i].goal_name);
+            }
+            // Map goalArray to an array of HTML elements
+            const newGoalArray = goalsArray.map((e) => {
+              const div = $("<div>");
+              const h6 = $("<h6>").text(e);
+              const button = $("<button>")
+                .text("حذف")
+                .addClass("removeGoal")
+                .data("id", e);
+              div.append(h6, button);
+              return div;
+            });
+
+            // Add the new HTML elements to goalInsertedDiv
+            goalsInsertedDiv.html(newGoalArray);
+            goalsInsertedDiv.show();
+          },
+        });
+
+        var sqlAreas =
+          "SELECT `area_name` FROM `pro_area` WHERE `project_name` = '" +
+          projectName +
+          "'";
+
+        $.ajax({
+          url: "../phpFile/show.php",
+          data: { sql: sqlAreas },
+          dataType: "json",
+          type: "post",
+          success: function (out) {
+            for (let i = 0; i < out.length; i++) {
+              areasArray.push(out[i].area_name);
+            }
+            // Check if area is already in areaArray
+
+            areasArray.push(area);
+
+            // Map AreaArray to an array of HTML elements
+            const newAreaArray = areasArray.map((e) => {
+              const div = $("<div>");
+              const h6 = $("<h6>").text(e);
+              const button = $("<button>")
+                .text("حذف")
+                .addClass("removeArea")
+                .data("id", e);
+              div.append(h6, button);
+              return div;
+            });
+
+            // Add the new HTML elements to areasInsertedDiv
+            areasInsertedDiv.html(newAreaArray);
+            areasInsertedDiv.show();
+          },
+        });
+
+        var sqlTargets =
+          "SELECT `target_group` FROM `targ_pro` WHERE `project_name` = '" +
+          projectName +
+          "'";
+
+        $.ajax({
+          url: "../phpFile/show.php",
+          data: { sql: sqlTargets },
+          dataType: "json",
+          type: "post",
+          success: function (out) {
+            for (let i = 0; i < out.length; i++) {
+              targetsArray.push(out[i].target_group);
+            }
+            const newTargetArray = targetsArray.map((e) => {
+              const div = $("<div>");
+              const h6 = $("<h6>").text(e);
+              const button = $("<button>")
+                .text("حذف")
+                .addClass("removeTarget")
+                .data("id", e);
+              div.append(h6, button);
+              return div;
+            });
+
+            // Add the new HTML elements to targetsInsertedDiv
+            targetsInsertedDiv.html(newTargetArray);
+            targetsInsertedDiv.show();
+          },
+        });
+
+        $("#tableDis").hide();
+      },
+      error: function () {
+        // show error message and hide buttons
+        $("#alert")
+          .text("حدث خطأ أثناء تحميل بيانات المشروع")
+          .addClass("alert-danger")
+          .removeClass("alert-success")
+          .show();
+        $("#add").hide();
+        $("#edit").hide();
+      },
+    });
+  });
+
+
 });
---
+++ controlPanal/users/users.js
@@ -0,0 +1,211 @@
+$(document).ready(function () {
+  if (
+    localStorage.getItem("login") == 0 ||
+    localStorage.getItem("login") == null
+  ) {
+    window.location.replace("../login.html");
+  }
+
+  function reload(sql) {
+    $.ajax({
+      url: "../phpFile/show.php",
+      data: { sql: sql },
+      dataType: "json",
+      type: "post",
+      success: function (data) {
+        ht =
+          "<tr> <th>الاسم</th> <th>كلمة المرور</th> <th>الصلاحيات</th> <th>الحذف</th><th>النعديل</th></tr>";
+        for (i = 0; i < data.length; i++) {
+          ht +=
+            "<tr><td>" +
+            data[i].username +
+            "</td><td>" +
+            data[i].password +
+            "</td><td>" +
+            data[i].permission +
+            "</td> <td><button data-id= '" +
+            data[i].username +
+            '\' class="remove-btn">حذف <i class="fa fa-remove" aria-hidden="true"></i></button></td> <td><button data-id= \'' +
+            data[i].id +
+            '\' class="edit-btn">تعديل <i class="fas fa-edit"></i></button></td></tr>';
+        }
+        $(".table").html(ht);
+      },
+    });
+  }
+
+  $("#nav i").click(function () {
+    $("#smallList").toggle(300);
+  });
+
+  $("#links img").click(function () {
+    window.location.replace("Home.html");
+  });
+
+  $("#nav button span").click(function () {
+    window.location.replace("../login.html");
+  });
+
+  $("#add").click(function () {
+    nameAdd = $("#name").val();
+    passAdd = $("#password").val();
+    pirAdd = $("#pir").val();
+    sqlAdd =
+      "INSERT INTO users (`username`, `password`, `permission`) VALUES ('" +
+      nameAdd +
+      "','" +
+      passAdd +
+      "','" +
+      pirAdd +
+      "')";
+
+    $.ajax({
+      url: "../phpFile/add.php",
+      data: { sqlAdd: sqlAdd },
+      type: "post",
+      success: function (out) {
+        alert(out);
+        sql = "SELECT * FROM `users`";
+        reload(sql);
+      },
+    });
+  });
+
+  $("#info button").prop("disabled", true);
+
+  $("#info button").css({
+    cursor: "auto",
+  });
+
+  $("#info button").hover(
+    function () {
+      $(this).css({
+        "background-color": "#C58940",
+        color: "white",
+      });
+    },
+    function () {
+      $(this).css({
+        "background-color": "transparent",
+        color: "#C58940",
+      });
+    }
+  );
+
+  $("#info input:nth-child(4),#info input:nth-child(2)").keyup(function () {
+    if (
+      $("#info input:nth-child(2)").val() == "" ||
+      $("#info input:nth-child(4)").val() == ""
+    ) {
+      $("#info button").prop("disabled", true);
+      $("#info button").css({
+        cursor: "auto",
+      });
+    } else {
+      $("#info button").prop("disabled", false);
+      $("#info button").css({
+        cursor: "pointer",
+      });
+    }
+  });
+
+  $("#tableDis input").keyup(function () {
+    val = $(this).val();
+    if (val == "") {
+      sql = "SELECT * FROM `users`";
+    } else {
+      sql =
+        "SELECT * FROM `users` WHERE `permission` like '%" +
+        val +
+        "%' || `username` like '%" +
+        val +
+        "%' || `password` like '%" +
+        val +
+        "%'";
+    }
+    reload(sql);
+  });
+
+  sql = "SELECT * FROM `users`";
+  reload(sql);
+
+  $("#table").on("click", ".remove-btn", function () {
+    let n = confirm("تأكيد الحذف");
+    if (n) {
+      id = $(this).data("id");
+      table = "users";
+      feild = "username";
+      sql = "DELETE FROM `" + table + "` WHERE `" + feild + "` = '" + id + "'";
+      $.ajax({
+        url: "../phpFile/remove.php",
+        data: { sql: sql },
+        type: "post",
+        success: function (out) {
+          reload("SELECT * FROM `users`");
+        },
+      });
+    }
+  });
+
+  let id;
+  $("#table").on("click", ".edit-btn", function () {
+    id = $(this).data("id");
+    sql = "SELECT * FROM `users` WHERE `id`='" + id + "'";
+    $.ajax({
+      url: "../phpFile/show.php",
+      data: { sql: sql },
+      type: "post",
+      dataType: "json",
+      success: function (out) {
+        name = out[0].username;
+        password = out[0].password;
+        pir = out[0].permission;
+
+        $("#name").val(name);
+        $("#password").prop("type", "text");
+        $("#password").val(password);
+        $("#pir").val(pir);
+
+        setTimeout(function () {
+          window.location.replace("users.html#hero");
+        }, 400);
+
+        $("#add").toggle();
+        $("#edit").toggle();
+      },
+    });
+  });
+
+  $("#edit").click(function () {
+    nameAdd = $("#name").val();
+    passAdd = $("#password").val();
+    pirAdd = $("#pir").val();
+    sqlup =
+      "UPDATE `users` SET `username`='" +
+      nameAdd +
+      "',`password`='" +
+      passAdd +
+      "',`permission`='" +
+      pirAdd +
+      "' WHERE id = '" +
+      id +
+      "'";
+    $.ajax({
+      url: "../phpFile/update.php",
+      data: { sqlup: sqlup },
+      type: "post",
+      success: function () {
+        $("#name").val("");
+        $("#password").prop("type", "password");
+        $("#password").val("");
+
+        $("#add").toggle();
+        $("#edit").toggle();
+        reload("SELECT * FROM `users`");
+        setTimeout(function () {
+          window.location.replace("users.html#table");
+        }, 400);
+      },
+    });
+  });
+});
